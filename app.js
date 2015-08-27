e8052c69919336f0e0daef57646b9d3e7f575af1
Taras Kalapun <t.kalapun@gmail.com>
2015-08-27 11:35:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-function getData(url, callback) {
+function getYAML(url, callback) {
   request = new XMLHttpRequest();
   request.open('GET', url, true);
   request.onload = function() {
     if (this.status >= 200 && this.status < 400){
       // Success!
-      data = JSON.parse(this.response);
+      data = yaml.safeLoad(this.response);
+      //data = JSON.parse(this.response);
       callback(data);
     } else {
       // We reached our target server, but it returned an error
@@ -72,8 +73,7 @@ function processTx(budget) {
   
   
 
-  getData(matches_url, function( matches_data ) {
-    var matches = yaml.safeLoad(matches_data);
+  getYAML(matches_url, function( matches ) {
     budget.loadMatches(matches);
     budget.parseData(data);
     showTxData(budget.expenses);
